8dc96b66154c1cbb901d22a7c7ce4af08e650c68
Andrew Williams <sobakasu@gmail.com>
2019-06-27 14:24:39 +0930

    Initial commit

---
+++ lib/mqtt/homie.rb
@@ -0,0 +1,8 @@
+require "mqtt/homie/version"
+
+module Mqtt
+  module Homie
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/mqtt/homie/version.rb
@@ -0,0 +1,5 @@
+module Mqtt
+  module Homie
+    VERSION = "0.1.0"
+  end
+end
